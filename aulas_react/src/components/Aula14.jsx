475dbedc54b00c97261384db6d6e15adceac2497
Rafaela <rafaelasouzasilva3009@gmail.com>
2026-03-26 13:09:40 -0300

    Fix: ajustando gitignore

---
+++ aulas_react/src/components/Aula14.jsx
@@ -0,0 +1,13 @@
+import { estilos } from "../style/Estilos"
+
+
+const Aula14 = () => {
+    return(
+        <div style={estilos.cardAula}>
+            <h2>Aula14 - CRUD com API</h2>
+            <h3>Criando uma CRUD utilizando API desenvolvida em Backend</h3> 
+        </div>
+    )
+}
+
+export default Aula14
